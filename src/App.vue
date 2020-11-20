58d0e55d557c2b6118f3fd0f8e978e5abae92914
tayyab <tayyabdev@outlook.com>
2020-11-20 08:43:09 +0000

    paths

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     
     <nav  style="background:#000000;opacity:0.9" class="navbar navbar-expand-lg">
-  <a class="navbar-brand" style="color:white;font-weight:bold" href="#">TayyabUniversity.com</a>
+  <a class="navbar-brand" style="color:#F9F7F3;font-weight:bold" href="#">TayyabUniversity.com</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -10,17 +10,17 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
     <ul class="navbar-nav mr-auto ">
-      <li class="nav-item active">
+      <li @click="goTo('/')"  class="nav-item active">
         <a :style="{color: $router.currentRoute.path=='/' ? '#F9F7F3':'#A1E8AF' }" style="color:white;" class="nav-link" href="#">Home <span v-if="$router.currentRoute.path=='/d'" class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
+      <li @click="goTo('/paths')" class="nav-item">
         <a :style="{color: $router.currentRoute.path=='/paths' ? '#F9F7F3':'#A1E8AF' }" style="color:white;font-weight:100" class="nav-link" href="#">Paths</a>
       </li>
-       <li class="nav-item">
+       <li @click="goTo('/submit')" class="nav-item">
         <a :style="{color: $router.currentRoute.path=='/submit' ? '#F9F7F3':'#A1E8AF' }" style="color:white;font-weight:100" class="nav-link" href="#">Submit</a>
       </li>
 
-       <li class="nav-item">
+       <li @click="goTo('/faq')" class="nav-item">
         <a :style="{color: $router.currentRoute.path=='/faq' ? '#F9F7F3':'#A1E8AF' }" style="color:white;font-weight:100" class="nav-link" href="#">FAQ</a>
       </li>
    
@@ -34,6 +34,17 @@
     <router-view />
   </div>
 </template>
+
+
+<script>
+export default {
+  methods:{
+    goTo(route){
+      this.$router.push(route);
+    }
+  }
+}
+</script>
 
 <style>
 #app {
--- src/views/pages/paths.vue
+++ src/views/pages/paths.vue
@@ -2,13 +2,52 @@
 <style scoped>
 
 
+#bg{
+    background-image: url('../../assets/pathspromo.jpg');
+}
 </style>
 
 
 <template>
     <div>
-        <h1>Paths</h1>
-    
+
+        <div id="#bg" style="width:100%;height:600px;position:relative;">
+            <div style="flex-flow:column;z-index:2;position:absolute;background:none;width:100%;height:500px;display:flex;align-items:center;justify-content:center;" >
+                
+                <div><h1 style="color:white;font-weight:bold;font-size:40px;background:none;width:80%;border-radius:5px;height:30%;opacity:1;font-family: 'M PLUS 1p', sans-serif;">
+                    Learning.Made.Easy
+                </h1></div>
+                 <div style="width:100%;text-align:center;"><p style="text-align:center;;color:white;font-weight:bold;font-size:20px;background:none;width:100%;border-radius:5px;height:30%;opacity:1;font-family: 'M PLUS 1p', sans-serif;">
+                     Dedicated online courses to help you become a competant software developer
+                </p></div>
+            </div>
+            <img style="width:100%;height:100%;z-index:-1;filter:blur(5px)" src="../../assets/pathspromo.jpg"/>
+
+            <div style="position:relative;bottom:30%;left:30%;width:600px;height:300px;background:black">
+                <div style="height:30%;width:100%;background:#171123;display:flex;align-items:center;justify-content:center;">
+                    <div style="height:100%;display:flex;align-items:center;justify-content:center;padding-top:10px;"><p style="color:white;font-family:'roboto';font-size:20px;">Sign up to my Email List</p></div>
+                </div>
+                <div style="flex-flow:column;height:69%;width:100%;background:#ffffff;display:flex;align-items:start;justify-content:;">
+                   
+                   <div style="width:100%;height:50%;background:none;display:flex;align-items:center;">
+                    <div class="input-group flex-nowrap" style="width:80%;width:100%;margin:30px;">   
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="addon-wrapping">@</span>
+                        </div>
+                         <input type="text" class="form-control" placeholder="Email" aria-label="Username" aria-describedby="addon-wrapping">
+                        </div>       
+                        </div>
+                        <div style="display:flex;align-items:center;justify-content:center;width:100%">
+                            <button type="button" class="btn btn-success">>Submit</button>
+                        </div>
+                    </div>
+                    
+                    </div>
+                    
+            </div>
+
+        <div style="height:3000px;"></div>
+
     </div>
 </template>
 
